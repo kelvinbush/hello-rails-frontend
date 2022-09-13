d670b791a282107f68e763660e50328f12e76ca1
Kelvin Bush <kelybush@gmail.com>
2022-09-13 15:47:08 +0300

    complete app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import {Provider} from 'react-redux';
+
+import store from './store/configure-store';
+
+import Greeting from './components/Greeting';
+
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
---
+++ src/store/configure-store.js
@@ -0,0 +1,1 @@
+import {    applyMiddleware,    combineReducers,    legacy_createStore as createStore,} from 'redux';import thunk from 'redux-thunk';import GreetingReducer from './greetings/greetings';const rootReducer = combineReducers({    greeting: GreetingReducer,});const store = createStore(    rootReducer,    applyMiddleware(        thunk,    ),);export default store
